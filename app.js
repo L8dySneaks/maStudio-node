f1680de40d0a539f13eceb7697d640e00ed8654e
Stephanie <steph@MacBook-Pro.home>
2020-09-02 01:34:29 -0400

    use appendFile to add another note to the text file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('test.txt', 'DONT FORGET add library changed to PR template');
+fs.writeFileSync('test.txt', 'add library changed to PR template\n');
+fs.appendFileSync('test.txt', 'update Docker files to copy after npm install');
