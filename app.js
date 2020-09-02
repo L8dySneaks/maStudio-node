5a52db381e763e08472b2dec210015350b58eab1
Stephanie <steph@MacBook-Pro.home>
2020-09-02 01:38:37 -0400

    add another list item to text file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 
 fs.writeFileSync('test.txt', 'add library changed to PR template\n');
-fs.appendFileSync('test.txt', 'update Docker files to copy after npm install');
+fs.appendFileSync('test.txt', 'update Docker files to copy after npm install\n');
+fs.appendFileSync('test.txt', 'write steps needing to be done for new image manager update');
